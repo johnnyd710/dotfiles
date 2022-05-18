4f584b21b7fbaab03166b1407e4cca51140dfd4b
johnnyd710 <19596966+johnnyd710@users.noreply.github.com>
2022-05-18 13:21:34 -0400

    added touch alias

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,6 +4,9 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
 
 # Aliases
+function touch {
+  if((Test-Path -Path ($args[0])) -eq $false) {Set-Content -Path ($args[0]) -Value ($null)} else {(Get-Item ($args[0])).LastWriteTime = Get-Date }
+}
 function Show-Itwin-Config {
   cat ~\repos\me.config\viewer.env.local
 }

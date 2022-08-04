30655da17471d2fb54df87cb42798cda72f3ff4a
johnnyd710 <19596966+johnnyd710@users.noreply.github.com>
2022-08-03 20:45:24 -0400

    set and clear dev env

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,19 +2,33 @@
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
+Import-Module -Name Terminal-Icons
+
+# Shorten my powershell path
+function prompt {'~$ ' + $(Get-Location | Split-Path -Leaf) + "> "}
 
 # Aliases
 function touch {
   if((Test-Path -Path ($args[0])) -eq $false) {Set-Content -Path ($args[0]) -Value ($null)} else {(Get-Item ($args[0])).LastWriteTime = Get-Date }
 }
-function Show-Itwin-Config {
-  cat ~\repos\me.config\viewer.env.local
+function Copy-Itwin-Config {
+  cp ~\repos\scripts\itwin\templates\.env.local .env.local
 }
 function Create-Cospace {
   ~\repos\scripts\itwin\create-cospace.ps1
 }
-function Show-Aliases {
-  cat $profile
+function Open-Notes {
+  code "C:\Users\John.DiMatteo\OneDrive - Bentley Systems, Inc\notes"
+}
+function Set-Dev-Env {
+  $Env:NODE_TLS_REJECT_UNAUTHORIZED='0'
+  $Env:IMJS_URL_PREFIX="dev-"
+  $Env:NODE_ENV="development"
+}
+function Clear-Dev-Env {
+  $Env:NODE_TLS_REJECT_UNAUTHORIZED=""
+  $Env:IMJS_URL_PREFIX=""
+  $Env:NODE_ENV=""
 }
 
 # fnm config

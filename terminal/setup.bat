9b5420d2fe7bd70820d5192cdbb32e4d29a18fd7
John DiMatteo <19596966+johnnyd710@users.noreply.github.com>
2023-08-02 09:56:39 -0400

    update setup file terminal

--- terminal/setup.bat
+++ terminal/setup.bat
@@ -1,4 +1,4 @@
 @echo off
 
-winget install --id=Microsoft.WindowsTerminal.Preview
+@REM winget install --id=Microsoft.WindowsTerminal.Preview
 mklink /H "%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json" ".\settings.json"

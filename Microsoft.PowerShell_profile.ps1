8f6ddd3fbac50fc5abd6389838eacebd08b5cc70
John DiMatteo <19596966+johnnyd710@users.noreply.github.com>
2024-04-02 10:25:38 -0400

    git branch name in ps prompt

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,14 +5,30 @@ Set-PSReadLineOption -EditMode Windows
 
 # Aliases
 function prompt {
+    $base = "PS "
     $path = "$($executionContext.SessionState.Path.CurrentLocation)"
     $userPrompt = "$('>' * ($nestedPromptLevel + 1)) "
 
-    Write-Host "`n" -NoNewline
+    Write-Host "`n$base" -NoNewline
 
     if (Test-Path .git) {
         Write-Host $path -NoNewline -ForegroundColor "green"
-        Write-BranchName
+        try {
+            $branch = git rev-parse --abbrev-ref HEAD
+
+            if ($branch -eq "HEAD") {
+                # we're probably in detached HEAD state, so print the SHA
+                $branch = git rev-parse --short HEAD
+                Write-Host " ($branch)" -ForegroundColor "red"
+            }
+            else {
+                # we're on an actual branch, so print it
+                Write-Host " ($branch)" -ForegroundColor "blue"
+            }
+        } catch {
+            # we'll end up here if we're in a newly initiated git repo
+            Write-Host " (no branches yet)" -ForegroundColor "yellow"
+        }
     }
     else {
         # we're not in a repo so don't bother displaying branch name/sha
